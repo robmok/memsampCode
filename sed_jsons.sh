29391f94c637c948a77f67504bbc000d209f926e
Robert Mok <robert.mok@love06.local>
2018-12-05 17:48:05 +0000

    update

--- sed_jsons.sh
+++ sed_jsons.sh
@@ -11,38 +11,56 @@ printf "Starting script sed_jsons.sh. This will add required info into json file
 for iSub in {01..33}; do
   subCounterP=`printf "%.2d" ${subCounter}` #zero pad subject number
   printf "Organising subject ${subCounterP} \n"
-
   subDir=${bidsDir}/sub-${subCounterP}
 
-#add 'TaskName' to epis
+  #anat
+  dir=${subDir}/anat
+  fname=`ls ${dir}/*.json 2> /dev/null`
+  for iFile in ${fname}; do
+    #edit "PatientSex" value to "" for ALL json files - anat, fmap, func
+    tmp=$(mktemp)
+    sed -e 's/"PatientSex": ""/"PatientSex": ""/' ${iFile} > ${tmp} && mv "$tmp" ${iFile} #this removes a control character from this "^P"
+    jq '.RepetitionTime = 2' ${iFile} > ${tmp} && mv "$tmp" ${iFile} #
+  done
 
-#edit "PatientSex" value to ""
+  #fmap
+  dir=${subDir}/fmap
+  fname=`ls ${dir}/*.json 2> /dev/null`
+  for iFile in ${fname}; do
+    tmp=$(mktemp)
+    sed -e 's/"PatientSex": ""/"PatientSex": ""/' ${iFile} > ${tmp} && mv "$tmp" ${iFile} #this removes a control character from this "^P"
+  done
+  # get TE1 from mag1, and add "EchoTime1" and "EchoTime2" to phasediff .json (TE2 already in 'EchoTime' in phasediff json)
+  fname=${subDir}/fmap/sub-${subCounterP}_magnitude1.json
+  TE1=`jq -r '.EchoTime' ${fname}` #get TE1
+  fname=${subDir}/fmap/sub-${subCounterP}_phasediff.json
+  tmp=$(mktemp)
+  jq '. + {"EchoTime2": .EchoTime} | del(.EchoTime)' ${fname} > ${tmp} && mv "$tmp" ${fname} #change TE to TE2, remove TE
+  jq --argjson TE1val ${TE1} '. + {EchoTime1: $TE1val}' ${fname} > ${tmp} && mv "$tmp" ${fname} # add TE1; argjson for number
 
-#fmap - get TE1 from mag1, and add "EchoTime1" and "EchoTime2" to phasediff .json (TE2 already in 'EchoTime' in phasediff json)
-
-fname=${subDir}/anat/
-
-
-
-#temp
-fname=${subDir}/func/sub-01_task-memsamp_run-01_bold
-
-#both work now
-tmp=$(mktemp)
-sed -e 's/"PatientSex": ""/"PatientSex": ""/' ${fname}.json > ${tmp} && mv "$tmp" ${fname}.json #this removes a control character from this "^P"
-jq --arg TaskName memsamp '. + {TaskName: $TaskName}' ${fname}.json > ${tmp} && mv "$tmp" ${fname}.json #works
-
-
-#TE1 and TE2 now
-
-
-
-
-#  ${subDir}/fmap
-#  ${subDir}/func
-
-
-
+  #EPIs - add TaskName, edit TR?
+  dir=${subDir}/func
+  fname=`ls ${dir}/*memsamp*.json 2> /dev/null`
+  for iFile in ${fname}; do
+    tmp=$(mktemp)
+    sed -e 's/"PatientSex": ""/"PatientSex": ""/' ${iFile} > ${tmp} && mv "$tmp" ${iFile} #this removes a control character from this "^P"
+    jq --arg TaskName memsamp '. + {TaskName: $TaskName}' ${iFile} > ${tmp} && mv "$tmp" ${iFile} #works
+    jq '.RepetitionTime = 2.8' ${iFile} > ${tmp} && mv "$tmp" ${iFile} #
+  done
+  fname=`ls ${dir}/*motionLoc*.json 2> /dev/null`
+  for iFile in ${fname}; do
+    tmp=$(mktemp)
+    sed -e 's/"PatientSex": ""/"PatientSex": ""/' ${iFile} > ${tmp} && mv "$tmp" ${iFile} #this removes a control character from this "^P"
+    jq --arg TaskName motionLocaliser '. + {TaskName: $TaskName}' ${iFile} > ${tmp} && mv "$tmp" ${iFile}
+    jq '.RepetitionTime = 2.8' ${iFile} > ${tmp} && mv "$tmp" ${iFile} #
+  done
+  fname=`ls ${dir}/*exemplarLoc*.json 2> /dev/null`
+  for iFile in ${fname}; do
+    tmp=$(mktemp)
+    sed -e 's/"PatientSex": ""/"PatientSex": ""/' ${iFile} > ${tmp} && mv "$tmp" ${iFile} #this removes a control character from this "^P"
+    jq --arg TaskName exemplarLocaliser '. + {TaskName: $TaskName}' ${iFile} > ${tmp} && mv "$tmp" ${iFile}
+    jq '.RepetitionTime = 2.8' ${iFile} > ${tmp} && mv "$tmp" ${iFile} #
+  done
 
   let subCounter=subCounter+1
 done
