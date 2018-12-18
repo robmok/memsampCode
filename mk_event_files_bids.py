15f5c215fe32438d9e16455f786f19f251d54c74
Robert Mok <robert.mok@love06.local>
2018-12-18 17:34:20 +0000

    update

--- mk_event_files_bids.py
+++ mk_event_files_bids.py
@@ -13,6 +13,7 @@ import pandas as pd
 
 bidsDir='/Users/robert.mok/Documents/Postdoc_ucl/memsamp_fMRI/memsampBids'
 eventsDir='/Users/robert.mok/Documents/Postdoc_ucl/memsamp_fMRI/orig_events'
+featDir='/Users/robert.mok/Documents/Postdoc_ucl/memsamp_fMRI/feat_timing'
 os.chdir(bidsDir)
 
 subs = range(1,34) #33 subs - range doesn't include last number
@@ -50,6 +51,49 @@ for iSub in subs:
         os.rename(iFile,  os.path.join(eventsDir, os.path.basename(iFile)))                               
         #save as iFile
         trials.to_csv(iFile,sep='\t', header=True, index=False)
+        
+        #for FSL timing file
+        conds=trials["direction"].sort_values().unique() #gets direction conditions
+        for iCond in conds:
+            # block-wise estimation of betas
+            # cue direction
+            tmp=trials.loc[(trials['trial_type'] == 'cue') & (trials['direction'] == iCond)]
+            cuetiming=tmp[['onset','duration']]
+            cuetiming.insert(2, 'value', 1) #stim value
+            fname1=os.path.join(featDir,os.path.splitext(os.path.basename(iFile))[0] + "_" + str(int(iCond)) + "cue_block.txt")
+            cuetiming.to_csv(fname1,sep='\t', header=False, index=False)
+            #feedback - based on cue direction
+            tmp=trials.loc[(trials['trial_type'] == 'feedback') & (trials['direction'] == iCond)]
+            feedtiming=tmp[['onset','duration']]
+            feedtiming.insert(2, 'value', 1) #stim value
+            fname1=os.path.join(featDir,os.path.splitext(os.path.basename(iFile))[0] + "_" + str(int(iCond)) + "feed_block.txt")
+            feedtiming.to_csv(fname1,sep='\t', header=False, index=False)
+        
+            # trial-wise estimation of betas
+            trlCnt=1
+            for iTrl in range(0,len(cuetiming)): #number of trials in each condition
+                # cue direction
+                cuetimingTrl=cuetiming.iloc[iTrl]
+                cuetimingTrl=cuetimingTrl.reset_index() #for 1 trial, saves as 1 column; here, reset
+                cuetimingTrl=pd.pivot_table(cuetimingTrl, columns=cuetimingTrl['index']) #use index from the resetted index above
+                cuetimingTrl = cuetimingTrl[['onset','duration','value']]
+                fname1=os.path.join(featDir,os.path.splitext(os.path.basename(iFile))[0] + "_" + str(int(iCond)) + "cue_trial" + str(trlCnt) + ".txt")
+                cuetimingTrl.to_csv(fname1,sep='\t', header=False, index=False)
+                # feedback - based on cue direction
+                feedtimingTrl=feedtiming.iloc[iTrl]
+                feedtimingTrl=feedtimingTrl.reset_index() #for 1 trial, saves as 1 column; here, reset
+                feedtimingTrl=pd.pivot_table(feedtimingTrl, columns=feedtimingTrl['index']) #use index from the resetted index above
+                feedtimingTrl = cuetimingTrl[['onset','duration','value']]
+                fname1=os.path.join(featDir,os.path.splitext(os.path.basename(iFile))[0] + "_" + str(int(iCond)) + "feed_trial" + str(trlCnt) + ".txt")
+                feedtimingTrl.to_csv(fname1,sep='\t', header=False, index=False)
+                trlCnt=trlCnt+1
+        
+        #********
+        #feedback - based on category presented (need to figure this out) - separate GLM
+        
+        
+        
+        
 
     #Localisers
     #motion loc
