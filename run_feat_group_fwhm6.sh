0ddea2e57deb3eb7e7b17f96e377cc308896bbe4
Robert Mok <robert.mok@love06.local>
2019-01-17 17:13:24 +0000

    update

---
+++ run_feat_group_fwhm6.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+wd='/Users/robert.mok/Documents/Postdoc_ucl/memsamp_fMRI'
+
+fsfDir=${wd}/memsampCode/feat_design_files
+fmriDir=${wd}/participant/fmriprep
+#codeDir=${wd}/memsampCode
+
+cd ${wd}
