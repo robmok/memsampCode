e48ef8bdd4c24198f57ff3dbe009456b4b8f6ff4
Robert Mok <robert.mok@love06.local>
2019-02-21 22:21:19 +0000

    update

--- run_3rdlvl_feat_group.sh
+++ run_3rdlvl_feat_group.sh
@@ -3,22 +3,20 @@ wd='/Users/robert.mok/Documents/Postdoc_ucl/memsamp_fMRI'
 fsfDir=${wd}/feat_design_files
 
 standardScript='memsamp_block_cope1_3rdlvl'
-standardScript='memsamp_block_cope1_3rdlvl_nofmaps'
-#for iCope in {1..13}; do
-#    sed -e s:cope1.feat/stats/cope1.nii.gz:cope${iCope}.feat/stats/cope1.nii.gz:g \
-#        -e s:cope1_fwhm6:cope${iCope}_fwhm6:g \
-#      <${fsfDir}/${standardScript}.fsf >${fsfDir}/run_memsamp_block_cope${iCope}_3rdlvl.fsf
-#  feat ${fsfDir}/run_memsamp_block_cope${iCope}_3rdlvl.fsf
-#done
-
-fwhm=6
-#standardScript="memsamp_block_cope1_3rdlvl_fwhm${fwhm}" #8
-#standardScript="memsamp_block_cope1_3rdlvl_fwhm${fwhm}_noTD" #8
-standardScript="memsamp_fsl_block_cope1_3rdlvl_fwhm${fwhm}" #6
-
 for iCope in {1..13}; do
     sed -e s:cope1.feat/stats/cope1.nii.gz:cope${iCope}.feat/stats/cope1.nii.gz:g \
-        -e s:cope1_fwhm${fwhm}:cope${iCope}_fwhm${fwhm}:g \
-      <${fsfDir}/${standardScript}.fsf >${fsfDir}/run_memsamp_block_cope${iCope}_3rdlvl_fwhm${fwhm}.fsf
-  feat ${fsfDir}/run_memsamp_block_cope${iCope}_3rdlvl_fwhm${fwhm}.fsf
+        -e s:cope1_fwhm6:cope${iCope}_fwhm6:g \
+      <${fsfDir}/${standardScript}.fsf >${fsfDir}/run_memsamp_block_cope${iCope}_3rdlvl.fsf
+  feat ${fsfDir}/run_memsamp_block_cope${iCope}_3rdlvl.fsf
 done
+
+#fwhm=6
+
+#standardScript="memsamp_fsl_block_cope1_3rdlvl_fwhm${fwhm}" #6
+
+#for iCope in {1..13}; do
+#    sed -e s:cope1.feat/stats/cope1.nii.gz:cope${iCope}.feat/stats/cope1.nii.gz:g \
+#        -e s:cope1_fwhm${fwhm}:cope${iCope}_fwhm${fwhm}:g \
+#      <${fsfDir}/${standardScript}.fsf >${fsfDir}/run_memsamp_block_cope${iCope}_3rdlvl_fwhm${fwhm}.fsf
+#  feat ${fsfDir}/run_memsamp_block_cope${iCope}_3rdlvl_fwhm${fwhm}.fsf
+#done
--- run_feat_main_T1space_trialwise.sh
+++ run_feat_main_T1space_trialwise.sh
@@ -4,13 +4,17 @@ wd='/Users/robert.mok/Documents/Postdoc_ucl/memsamp_fMRI'
 fsfDir=${wd}/feat_design_files
 dataDir=${wd}/fmriprep_output/fmriprep
 
-fwhm=2 #6/8
-standardScript="memsamp_run-01_block_fwhm2_T1"
-#standardScript="memsamp_run-01_block_fwhm2_T1_brainmask" #very similar if not same
+fwhm=2
+standardScript="memsamp_run-01_trial_fwhm2_T1"
+
+# single trial in T1 space
+# - code for each trial as an EV - looks like 7 trials per run (even run 4)
+# - 12 conds * 7 trials = 84 EVs PLUS feedback.
+# - all 84 as contrasts, plus 1 for feedback
 
 cd ${wd}
 while read subject; do
-   for iRun in {1..3}; do
+   for iRun in 1; do
      epi_file="${dataDir}/${subject}/func/${subject}_task-memsamp_run-0${iRun}_space-T1w_desc-preproc_bold"
      vols=`fslnvols ${epi_file}`
      voxels=`fslstats ${epi_file} -v | awk '{print $1}'`
@@ -21,8 +25,8 @@ while read subject; do
        -e s:"set fmri(smooth) 2.0":"set fmri(smooth) ${fwhm}":g \
      	-e s:"set fmri(npts) 262":"set fmri(npts) ${vols}":g \
      	-e s:"set fmri(totalVoxels) 85235150":"set fmri(totalVoxels) ${voxels}":g \
-       <${fsfDir}/${standardScript}.fsf >${fsfDir}/run_memsamp_run-0${iRun}_block_T1_fwhm${fwhm}_${subject}.fsf
-     feat ${fsfDir}/run_memsamp_run-0${iRun}_block_T1_fwhm${fwhm}_${subject}.fsf
+       <${fsfDir}/${standardScript}.fsf >${fsfDir}/run_memsamp_run-0${iRun}_trial_T1_fwhm${fwhm}_${subject}.fsf
+     feat ${fsfDir}/run_memsamp_run-0${iRun}_trial_T1_fwhm${fwhm}_${subject}.fsf
    done #for iRun
   #extra runs
   if [ "$subject" = "sub-09" ] || [ "$subject" = "sub-12" ] || [ "$subject" = "sub-16" ] || [ "$subject" = "sub-26" ]; then
@@ -37,7 +41,7 @@ while read subject; do
       -e s:"set fmri(smooth) 2.0":"set fmri(smooth) ${fwhm}":g \
       -e s:"set fmri(npts) 262":"set fmri(npts) ${vols}":g \
       -e s:"set fmri(totalVoxels) 85235150":"set fmri(totalVoxels) ${voxels}":g \
-      <${fsfDir}/${standardScript}.fsf >${fsfDir}/run_memsamp_run-0${iRun}_block_T1_fwhm${fwhm}_${subject}.fsf
-      feat ${fsfDir}/run_memsamp_run-0${iRun}_block_T1_fwhm${fwhm}_${subject}.fsf
+      <${fsfDir}/${standardScript}.fsf >${fsfDir}/run_memsamp_run-0${iRun}_trial_T1_fwhm${fwhm}_${subject}.fsf
+      feat ${fsfDir}/run_memsamp_run-0${iRun}_trial_T1_fwhm${fwhm}_${subject}.fsf
   fi #if ["subject" == "sub-09"]...
-done < ${fsfDir}/subject_list_1.txt #while read subject; do
+done < ${fsfDir}/subject_list_4.txt #while read subject; do
