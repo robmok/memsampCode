0caa5b4752938f4285235a17742d41725e836377
Robert Mok <robert.mok@love06.local>
2019-03-01 18:38:19 +0000

    update

--- mvpa_memsamp.py
+++ mvpa_memsamp.py
@@ -15,8 +15,8 @@ import numpy as np
 import clarte as cl
 import pandas as pd
 import matplotlib.pyplot as plt
-import nilearn.plotting as nip
-import nibabel as nib
+#import nilearn.plotting as nip
+#import nibabel as nib
 
 #roiDir='/Users/robert.mok/Documents/Postdoc_ucl/memsamp_fMRI/rois'
 featDir='/Users/robert.mok/Documents/Postdoc_ucl/memsamp_fMRI/memsampFeat'
@@ -24,14 +24,7 @@ bidsDir='/Users/robert.mok/Documents/Postdoc_ucl/memsamp_fMRI/memsampBids'
 
 os.chdir(featDir)
 
-# - could first try the LOO one with 'trials'. but check if it takes run into account (i think it does).
-# - see if easy to use block as test set
-
-#data
-#sub-01_run-01_block_T1_fwhm2.feat
-# - looks like  i need to load in the 2 blocks of training data, then single trials for the remaining block (test)
-#sub-01_run-01_trial_T1_fwhm2.feat
-
+# - first try the LOO one with 'trials'. then load in blocks
 
 #sub-01_task-memsamp_run-01_events.tsv #in bidsdir
 # - load in this
@@ -42,35 +35,51 @@ os.chdir(featDir)
 
 iSub=1
 subNum=f'{iSub:02d}'
-iRun=1
-
-condPath=os.path.join(bidsDir, 'sub-' + subNum, 'func','sub-' + subNum + '_task-memsamp_run-0' + str(iRun) +'_events.tsv')
-
-#temp path for laptop
-condPath='/Users/robertmok/Documents/Postdoc_ucl/sub-01_task-memsamp_run-01_events.tsv'
-dfCond=pd.read_csv(condPath, sep='\t')
-dfCond = dfCond[dfCond['trial_type']=='cue'] #remove feedback trials
-
-#add run number
-iRun=1
-dfCond['run']=pd.Series(np.ones((len(dfCond)))*iRun,index=dfCond.index)
-#dfCond.loc[:,'run2']=pd.Series(np.ones((len(dfCond)))*iRun,index=dfCond.index)
-
-# add path to match cue condition and trial number - cope1:7 is dir0, trial1:7
-# /Users/robert.mok/Documents/Postdoc_ucl/memsamp_fMRI/memsampFeat/sub-01_run-01_trial_T1_fwhm0.feat/stats/cope1.nii.gz
-
-conds=dfCond.direction.unique
-iCope=1
-for iCond in conds:
-    for iTrial in range(1,8): #calculate cope number
-        #make a list and append to it
-        imPath=os.path.join(featDir, 'sub-' + subNum + '_run-0' + str(iRun) +'_trial_T1_fwhm0.feat','stats','cope' + (str(iCope)) + '.nii.gz')
 
 
-        iCope=iCope+1
+#just to get the df structure (Better way to do this? maybe just an empty df? line 44)
+condPath=os.path.join(bidsDir, 'sub-01', 'func','sub-01' + '_task-memsamp_run-01_events.tsv')
+
+df=pd.read_csv(condPath, sep='\t')
+
+dfCond=pd.DataFrame(columns=list(df)) 
+for iRun in range(1,4):
+    condPath=os.path.join(bidsDir, 'sub-' + subNum, 'func','sub-' + subNum + '_task-memsamp_run-0' + str(iRun) +'_events.tsv')
+    #temp path for laptop
+    #condPath='/Users/robertmok/Documents/Postdoc_ucl/sub-01_task-memsamp_run-01_events.tsv'
+
+    df=pd.read_csv(condPath, sep='\t')
+    df = df[df['trial_type']=='cue'] #remove feedback trials
+    df['run']=pd.Series(np.ones((len(df)))*iRun,index=df.index) #add run number
+    #df.loc[:,'run2']=pd.Series(np.ones((len(df)))*iRun,index=df.index)
+    
+    # add path to match cue condition and trial number - cope1:7 is dir0, trial1:7   
+    conds=df.direction.unique()
+    conds.sort()
+    copeNum=1 #counter
+    imPath=[]
+    for iCond in conds:
+        for iTrial in range(1,8): #calculate cope number
+            #make a list and append to it
+            imPath.append(os.path.join(featDir, 'sub-' + subNum + '_run-0' + str(iRun) +'_trial_T1_fwhm0.feat','stats','cope' + (str(copeNum)) + '.nii.gz'))
+            copeNum=copeNum+1
+    df['imPath']=pd.Series(imPath,index=df.index) # temp df for this run
+    dfCond = dfCond.append(df) #append to main df
 
 
-#how to load in runs into dfCond? can it be multidimensional df? or hardcode nRuns?
+
+#4 runs
+if iSub in {9,12,16,26}:
+    iRun = 4
+
+
+
+
+
+
+
+
+
         
         
 #%%
