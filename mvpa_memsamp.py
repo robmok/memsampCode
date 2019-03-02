9456cc52cfbdeb30cbe7de10360a4bf2d5ee8021
Robert Mok <rob1031@gmail.com>
2019-03-02 16:38:11 +0000

    update

--- mvpa_memsamp.py
+++ mvpa_memsamp.py
@@ -12,7 +12,7 @@ import glob
 import numpy as np
 #np.set_printoptions(precision=2, suppress=True) # Set numpy to print only 2 decimal digits for neatness
 #from nilearn import image # Import image processing tool
-import clarte as cl
+#import clarte as cl
 import pandas as pd
 import matplotlib.pyplot as plt
 #import nilearn.plotting as nip
@@ -23,6 +23,8 @@ featDir='/Users/robert.mok/Documents/Postdoc_ucl/memsamp_fMRI/memsampFeat'
 bidsDir='/Users/robert.mok/Documents/Postdoc_ucl/memsamp_fMRI/memsampBids'
 
 os.chdir(featDir)
+
+#%%
 
 # - first try the LOO one with 'trials'. then load in blocks
 
@@ -36,24 +38,27 @@ os.chdir(featDir)
 iSub=1
 subNum=f'{iSub:02d}'
 
+dfCond=pd.DataFrame() #main df with all runs
 
-#just to get the df structure (Better way to do this? maybe just an empty df? line 44)
-condPath=os.path.join(bidsDir, 'sub-01', 'func','sub-01' + '_task-memsamp_run-01_events.tsv')
 
-df=pd.read_csv(condPath, sep='\t')
+if iSub in {9,12,16,26}:
+    runs = range(1,5) #4 runs
+else:
+    runs = range(1,4) #3 runs
 
-dfCond=pd.DataFrame(columns=list(df)) 
-for iRun in range(1,4):
-    condPath=os.path.join(bidsDir, 'sub-' + subNum, 'func','sub-' + subNum + '_task-memsamp_run-0' + str(iRun) +'_events.tsv')
+for iRun in runs:
+    condPath=os.path.join(bidsDir, 'sub-' + subNum, 'func','sub-' + subNum + 
+                          '_task-memsamp_run-0' + str(iRun) +'_events.tsv')
     #temp path for laptop
-    #condPath='/Users/robertmok/Documents/Postdoc_ucl/sub-01_task-memsamp_run-01_events.tsv'
-
-    df=pd.read_csv(condPath, sep='\t')
-    df = df[df['trial_type']=='cue'] #remove feedback trials
-    df['run']=pd.Series(np.ones((len(df)))*iRun,index=df.index) #add run number
-    #df.loc[:,'run2']=pd.Series(np.ones((len(df)))*iRun,index=df.index)
+    #condPath='/Users/robertmok/Documents/Postdoc_ucl/sub-01_task-memsamp_run-0' + str(iRun) + '_events.tsv'
     
-    # add path to match cue condition and trial number - cope1:7 is dir0, trial1:7   
+    # df to load in and organise run-wise data
+    df = pd.read_csv(condPath, sep='\t')
+    df = df[df['trial_type']=='cue'] #remove feedback trials
+    df['run'] = pd.Series(np.ones((len(df)))*iRun,index=df.index) #add run number
+    #df.loc[:,'run2']=pd.Series(np.ones((len(df)))*iRun,index=df.index) #alt way - better/worse?
+    
+    # add path to match cue condition and trial number - cope1:7 is dir0 trial1:7   
     conds=df.direction.unique()
     conds.sort()
     copeNum=1 #counter
@@ -61,26 +66,12 @@ for iRun in range(1,4):
     for iCond in conds:
         for iTrial in range(1,8): #calculate cope number
             #make a list and append to it
-            imPath.append(os.path.join(featDir, 'sub-' + subNum + '_run-0' + str(iRun) +'_trial_T1_fwhm0.feat','stats','cope' + (str(copeNum)) + '.nii.gz'))
+            imPath.append(os.path.join(featDir, 'sub-' + subNum + '_run-0'
+                                       + str(iRun) +'_trial_T1_fwhm0.feat',
+                                       'stats','cope' + (str(copeNum)) + '.nii.gz'))
             copeNum=copeNum+1
-    df['imPath']=pd.Series(imPath,index=df.index) # temp df for this run
+    df['imPath']=pd.Series(imPath,index=df.index)
     dfCond = dfCond.append(df) #append to main df
-
-
-
-#4 runs
-if iSub in {9,12,16,26}:
-    iRun = 4
-
-
-
-
-
-
-
-
-
-        
         
 #%%
 
