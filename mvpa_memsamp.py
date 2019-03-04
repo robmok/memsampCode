0542ae3c6fd8c669b56ae031c105bc40f098aade
Robert Mok <robert.mok@love06.local>
2019-03-04 15:04:35 +0000

    added to roi_merge script, make merged visual and ips masks

--- mvpa_memsamp.py
+++ mvpa_memsamp.py
@@ -24,61 +24,61 @@ bidsDir='/Users/robert.mok/Documents/Postdoc_ucl/memsamp_fMRI/memsampBids'
 
 os.chdir(featDir)
 
-#%%
+#%% load in trial log and append image paths
 
 # - first try the LOO one with 'trials'. then load in blocks
+    # - load in sub-01_task-memsamp_run-01_events.tsv #in bidsdi
+    # - append run number
+    # - append path to image - match 0:30:270 degrees to condition 1:12, trialwise (N.B. cope number is not the same for trialwise! 7 trials)
+    # - load in all 3 runs then merge the 3 dfs
 
-#sub-01_task-memsamp_run-01_events.tsv #in bidsdir
-# - load in this
-# - append run number
-# - append path to image - match 0:30:270 degrees to condition 1:12, trialwise (N.B. cope number is not the same for trialwise! 7 trials)
-
-# - load in all 3 runs then merge the 3 dfs
-
-iSub=1
-subNum=f'{iSub:02d}'
-
-dfCond=pd.DataFrame() #main df with all runs
-
-
-if iSub in {9,12,16,26}:
-    runs = range(1,5) #4 runs
-else:
-    runs = range(1,4) #3 runs
-
-for iRun in runs:
-    condPath=os.path.join(bidsDir, 'sub-' + subNum, 'func','sub-' + subNum + 
-                          '_task-memsamp_run-0' + str(iRun) +'_events.tsv')
-    #temp path for laptop
-    #condPath='/Users/robertmok/Documents/Postdoc_ucl/sub-01_task-memsamp_run-0' + str(iRun) + '_events.tsv'
+for iSub in range(1,34):
+    subNum=f'{iSub:02d}'
+    dfCond=pd.DataFrame() #main df with all runs
+    if iSub in {9,12,16,26}:
+        runs = range(1,5) #4 runs
+    else:
+        runs = range(1,4) #3 runs
     
-    # df to load in and organise run-wise data
-    df = pd.read_csv(condPath, sep='\t')
-    df = df[df['trial_type']=='cue'] #remove feedback trials
-    df['run'] = pd.Series(np.ones((len(df)))*iRun,index=df.index) #add run number
-    #df.loc[:,'run2']=pd.Series(np.ones((len(df)))*iRun,index=df.index) #alt way - better/worse?
+    for iRun in runs:
+        condPath=os.path.join(bidsDir, 'sub-' + subNum, 'func','sub-' + subNum + 
+                              '_task-memsamp_run-0' + str(iRun) +'_events.tsv')
+        #temp path for laptop
+        #condPath='/Users/robertmok/Documents/Postdoc_ucl/sub-01_task-memsamp_run-0' + str(iRun) + '_events.tsv'
+        
+        # df to load in and organise run-wise data
+        df = pd.read_csv(condPath, sep='\t')
+        df = df[df['trial_type']=='cue'] #remove feedback trials
+        df['run'] = pd.Series(np.ones((len(df)))*iRun,index=df.index) #add run number
+        #df.loc[:,'run2']=pd.Series(np.ones((len(df)))*iRun,index=df.index) #alt way - better/worse?
+        
+        # add path to match cue condition and trial number - cope1:7 is dir0 trial1:7   
+        conds=df.direction.unique()
+        conds.sort()
+        copeNum=1 #counter
+        imPath=[]
+        for iCond in conds:
+            for iTrial in range(1,8): #calculate cope number
+                #make a list and append to it
+                imPath.append(os.path.join(featDir, 'sub-' + subNum + '_run-0'
+                                           + str(iRun) +'_trial_T1_fwhm0.feat',
+                                           'stats','cope' + (str(copeNum)) + '.nii.gz'))
+                copeNum=copeNum+1
+        df['imPath']=pd.Series(imPath,index=df.index)
+        dfCond = dfCond.append(df) #append to main df
+    print('subject %s, length of df %s' % (subNum, len(dfCond)))
     
-    # add path to match cue condition and trial number - cope1:7 is dir0 trial1:7   
-    conds=df.direction.unique()
-    conds.sort()
-    copeNum=1 #counter
-    imPath=[]
-    for iCond in conds:
-        for iTrial in range(1,8): #calculate cope number
-            #make a list and append to it
-            imPath.append(os.path.join(featDir, 'sub-' + subNum + '_run-0'
-                                       + str(iRun) +'_trial_T1_fwhm0.feat',
-                                       'stats','cope' + (str(copeNum)) + '.nii.gz'))
-            copeNum=copeNum+1
-    df['imPath']=pd.Series(imPath,index=df.index)
-    dfCond = dfCond.append(df) #append to main df
         
 #%%
 
-subs=range(1,34)
-#for iSub in subs:
 
-iRun=1
-iCope=1
 
-datDir=os.path.join(featDir, 'sub-' + subNum + '_run-0' + str(iRun) +'_block_T1_fwhm2.feat/stats/cope' + str(iCope) + '.nii.gz')
+
+
+
+
+
+
+
+
+
--- roi_merge_lr_vd.sh
+++ roi_merge_lr_vd.sh
@@ -4,7 +4,10 @@ wd='/Users/robert.mok/Documents/Postdoc_ucl/memsamp_fMRI'
 roiDir=${wd}/rois
 fsfDir=${wd}/feat_design_files
 
+echo "Running roi_merge_kr.vs.sh"
+
 while read subject; do
+  echo "Running ${subject}"
   #make bilateral mask (left + right)
   for iRoi in 'V1v' 'V1d'  'V2v' 'V2d'  'V3v'  'V3d'  'hV4' 'V01' 'V02' 'PHC1' 'PHC2' 'MST' 'hMT' 'L02' 'L01' 'V3b' 'V3a' 'IPS0' 'IPS1' 'IPS2' 'IPS3' 'IPS4' 'IPS5' 'SPL1' 'FEF'; do
     fslmaths ${roiDir}/${subject}_${iRoi}_lh.nii.gz -add ${roiDir}/${subject}_${iRoi}_rh.nii.gz ${roiDir}/${subject}_${iRoi}_lrh.nii.gz
@@ -15,4 +18,34 @@ while read subject; do
     fslmaths ${roiDir}/${subject}_${iRoi}v_rh.nii.gz -add ${roiDir}/${subject}_${iRoi}d_rh.nii.gz ${roiDir}/${subject}_${iRoi}vd_rh.nii.gz #right vd
     fslmaths ${roiDir}/${subject}_${iRoi}v_lrh.nii.gz -add ${roiDir}/${subject}_${iRoi}d_lrh.nii.gz ${roiDir}/${subject}_${iRoi}vd_lrh.nii.gz #bilateral vd
   done
+  #make visual and IPS merged rois
+  #list the files to merge (txt is replaced over subs, not saved)
+  rm ${roiDir}/visual_rois_lh.txt ${roiDir}/visual_rois_rh.txt ${roiDir}/ips_rois_lh.txt ${roiDir}/ips_rois_rh.txt
+  for iRoi in 'V1v' 'V1d'  'V2v' 'V2d'  'V3v'  'V3d'  'hV4' 'V01' 'V02' 'PHC1' 'PHC2' 'MST' 'hMT' 'L02' 'L01' 'V3b'; do
+    echo -n "${roiDir}/${subject}_${iRoi}_lh.nii.gz -add " >> ${roiDir}/visual_rois_lh.txt #note the -n (for no new line) and space at the end of ""
+    echo -n "${roiDir}/${subject}_${iRoi}_rh.nii.gz -add " >> ${roiDir}/visual_rois_rh.txt
+  done
+  iRoi='V3a' #last no '-add'
+  echo -n "${roiDir}/${subject}_${iRoi}_lh.nii.gz " >> ${roiDir}/visual_rois_lh.txt
+  echo -n "${roiDir}/${subject}_${iRoi}_rh.nii.gz " >> ${roiDir}/visual_rois_rh.txt
+  for iRoi in 'IPS0' 'IPS1' 'IPS2' 'IPS3' 'IPS4' 'IPS5'; do
+    echo -n "${roiDir}/${subject}_${iRoi}_lh.nii.gz -add " >> ${roiDir}/ips_rois_lh.txt
+    echo -n "${roiDir}/${subject}_${iRoi}_rh.nii.gz -add " >> ${roiDir}/ips_rois_rh.txt
+  done
+  iRoi='SPL1' #last no '-add'
+  echo -n "${roiDir}/${subject}_${iRoi}_lh.nii.gz " >> ${roiDir}/ips_rois_lh.txt
+  echo -n "${roiDir}/${subject}_${iRoi}_rh.nii.gz " >> ${roiDir}/ips_rois_rh.txt
+
+  #run fslmaths - L/R/bilateral
+  roiList=`cat ${roiDir}/visual_rois_lh.txt`
+  fslmaths ${roiList} ${roiDir}/${subject}_visRois_lh.nii.gz
+  roiList=`cat ${roiDir}/visual_rois_rh.txt`
+  fslmaths ${roiList} ${roiDir}/${subject}_visRois_rh.nii.gz
+  fslmaths ${roiDir}/${subject}_visRois_lh.nii.gz -add ${roiDir}/${subject}_visRois_rh.nii.gz ${roiDir}/${subject}_visRois_lrh.nii.gz
+  roiList=`cat ${roiDir}/ips_rois_lh.txt`
+  fslmaths ${roiList} ${roiDir}/${subject}_ipsRois_lh.nii.gz
+  roiList=`cat ${roiDir}/ips_rois_rh.txt`
+  fslmaths ${roiList} ${roiDir}/${subject}_ipsRois_rh.nii.gz
+  fslmaths ${roiDir}/${subject}_ipsRois_lh.nii.gz -add ${roiDir}/${subject}_ipsRois_rh.nii.gz ${roiDir}/${subject}_ipsRois_lrh.nii.gz
+
 done < ${fsfDir}/subject_list_full.txt
