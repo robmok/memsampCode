7f55463cd2e1ca1b30732455b7173afadb6896a6
Robert Mok <robert.mok@love06.local>
2019-03-29 16:59:12 +0000

    update

--- run_searchlight_1.sh
+++ run_searchlight_1.sh
@@ -2,7 +2,7 @@
 mainDir='/Users/robert.mok/Documents/Postdoc_ucl/memsamp_fMRI'
 
 #love01
-#mainDir='/home/robmok/Documents/memsamp_fMRI' #love01
+mainDir='/home/robmok/Documents/memsamp_fMRI' #love01
 
 codeDir=${mainDir}/'memsampCode'
 tmpScrDir=${mainDir}/'mvpaTmpScripts'
