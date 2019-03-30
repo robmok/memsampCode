629a826ee04dd9bb2b119c823e93b763a18b6074
Robert Mok <robert.mok@love06.local>
2019-03-30 15:43:16 +0000

    update

--- run_searchlight_2.sh
+++ run_searchlight_2.sh
@@ -11,12 +11,18 @@ tmpScrDir=${mainDir}/'mvpaTmpScripts'
 
 
 
-# # dir cope 6mm, fwhmNone
+# # dir cope 6mm, fwhm1-  not sure why did 1 here, do none below
 # sed -e s:"#mainDir":"mainDir":g \
 #     -e s:"slSiz=5":"slSiz=6":g \
 #     -e s:"decodeFeature = '12-way'":"decodeFeature = 'dir'":g \
 #   < ${codeDir}/mvpa_searchlight_memsamp_blocks.py > ${tmpScrDir}/mvpa_searchlight_memsamp_blocks1.py
 # python ${tmpScrDir}/mvpa_searchlight_memsamp_blocks1.py
+
+# dir tstat 6mm cope, fwhmNone
+sed -e s:"#mainDir":"mainDir":g \
+    -e s:"decodeFeature = 'subjCat'":"decodeFeature = 'dir'":g \
+  < ${codeDir}/mvpa_searchlight_memsamp_blocks.py > ${tmpScrDir}/mvpa_searchlight_memsamp_blocks1.py
+python ${tmpScrDir}/mvpa_searchlight_memsamp_blocks1.py
 
 # dir tstat 6mm, fwhmNone
 sed -e s:"#mainDir":"mainDir":g \
@@ -24,7 +30,6 @@ sed -e s:"#mainDir":"mainDir":g \
     -e s:'cope':'tstat':g \
   < ${codeDir}/mvpa_searchlight_memsamp_blocks.py > ${tmpScrDir}/mvpa_searchlight_memsamp_blocks1.py
 python ${tmpScrDir}/mvpa_searchlight_memsamp_blocks1.py
-
 
 #crossnobis
 # ori cope 6mm, fwhmNone
