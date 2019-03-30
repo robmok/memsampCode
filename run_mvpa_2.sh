74e895c43de35781b8e1d0abb0da228128f4a512
Robert Mok <robert.mok@love06.local>
2019-03-30 01:00:32 +0000

    update

--- run_mvpa_2.sh
+++ run_mvpa_2.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 # Rois
 
-# mainDir='/Users/robert.mok/Documents/Postdoc_ucl/memsamp_fMRI'
-mainDir='/home/robmok/Documents/memsamp_fMRI' #love01
+mainDir='/Users/robert.mok/Documents/Postdoc_ucl/memsamp_fMRI'
+#mainDir='/home/robmok/Documents/memsamp_fMRI' #love01
 
 codeDir=${mainDir}/'memsampCode'
 tmpScrDir=${mainDir}/'mvpaTmpScripts'
