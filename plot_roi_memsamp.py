2547de48624a7801e3b9fc961164c85a6df544a8
Robert Mok <robert.mok@love06.local>
2019-04-04 17:13:40 +0100

    update

--- plot_roi_memsamp.py
+++ plot_roi_memsamp.py
@@ -9,19 +9,20 @@ Created on Thu Apr  4 16:00:52 2019
 import os
 import numpy as np
 import pandas as pd
-from matplotlib.pyplot import plot as plt
-mainDir='/Users/robert.mok/Documents/Postdoc_ucl/memsamp_fMRI'
+#from matplotlib.pyplot import plot as plt
 
+roiDir='/Users/robert.mok/Documents/Postdoc_ucl/memsamp_fMRI/mvpa_roi/'
+#roiDir='/Users/robert.mok/Documents/Postdoc_ucl/memsamp_fMRI/mvpa_roi/bilateral'
 
 imDat    = 'cope' # cope or tstat images
 normMeth = 'noNorm' # 'niNormalised', 'demeaned', 'demeaned_stdNorm', 'noNorm' # demeaned_stdNorm - dividing by std does work atm
-distMeth = 'crossNobis' # 'svm', 'crossNobis'
-trainSetMeth = 'block' # 'trials' or 'block' 
+distMeth = 'svm' # 'svm', 'crossNobis'
+trainSetMeth = 'trials' # 'trials' or 'block' 
 fwhm = None # optional smoothing param - 1, or None
 
-decodeFeature = 'subjCat' # '12-way' (12-way dir decoding - only svm), 'dir' (opposite dirs), 'ori' (orthogonal angles)
+decodeFeature = '12-way' # '12-way' (12-way dir decoding - only svm), 'dir' (opposite dirs), 'ori' (orthogonal angles)
 
-df=pd.read_pickle((os.path.join(mainDir, 'mvpa_roi', 'roi_' + decodeFeature + 'Decoding_' +
+df=pd.read_pickle((os.path.join(roiDir, 'roi_' + decodeFeature + 'Decoding_' +
                                 distMeth + '_' + normMeth + '_'  + trainSetMeth + 
                                 '_fwhm' + str(fwhm) + '_' + imDat + '.pkl')))
 #%%
@@ -30,4 +31,6 @@ df=pd.read_pickle((os.path.join(mainDir, 'mvpa_roi', 'roi_' + decodeFeature + 'D
 #p = avg_time.plot(figsize=15,5),legend=False,kind="bar",rot=45,color="blue",fontsize=16,yerr=std);
 
 stdAll = df.iloc[0:33,:].std()/np.sqrt(33)
-df.iloc[0:33,:].mean().plot(figsize=(15,5),kind="bar",yerr=stdAll)
+ax=df.iloc[0:33,:].mean().plot(figsize=(15,5),kind="bar",yerr=stdAll)
+#ax=df.iloc[0:33,:].mean().plot(figsize=(15,5),kind="bar",yerr=stdAll,ylim=(.5,.537))
+#ax=df.iloc[0:33,:].mean().plot(figsize=(15,5),kind="bar",yerr=stdAll,ylim=(1/12,0.09))
