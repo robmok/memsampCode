4584564752c46a824dec3ffa9d26c9ce65ec62ac
Robert Mok <robert.mok@love06.local>
2019-04-04 22:29:04 +0100

    update

--- mvpa_memsamp.py
+++ mvpa_memsamp.py
@@ -39,7 +39,7 @@ fwhm = None # optional smoothing param - 1, or None
 
 # stimulus decoding: '12-way' (12-way dir decoding - only svm), '12-way-all' (output single decoder for each dir vs all), 'dir' (opposite dirs), 'ori' (orthogonal angles)
 # category: 'objCat' (objective catgeory), 'subjCat' 
-decodeFeature = 'subjCat' 
+decodeFeature = 'subjCat-all' 
 
 #%%
 # =============================================================================
@@ -245,7 +245,8 @@ for iSub in range(1,nSubs+1):
             cvAcc = cvAccTmp #save all pairs
         
         dfDecode[roi].iloc[iSub-1]=cvAcc #store to main df
-        print('ROI: %s, Sub-%s %s measure = %0.3f' % (roi, subNum, distMeth, cvAcc))    
+        if not (decodeFeature=="12-way-all")|(decodeFeature=="subjCat-all"): 
+            print('ROI: %s, Sub-%s %s measure = %0.3f' % (roi, subNum, distMeth, cvAcc))    
 #compute t-test, append to df
 if distMeth == 'svm':
     chance = 1/len(np.unique(y_indexed))
