2010d59fdd5d07f7d527811d61047cdeaed5d60e
Robert Mok <robert.mok@love06.local>
2019-04-08 15:01:30 +0100

    update

--- run_mvpa_2.sh
+++ run_mvpa_2.sh
@@ -16,7 +16,7 @@ tmpScrDir=${mainDir}/'mvpaTmpScripts'
 # #subjCat-all -  #svm cope/svm tstat/crossNobis trials/blocks
 #
 # #svm trials cope
-python ${codeDir}/mvpa_memsamp.py
+# python ${codeDir}/mvpa_memsamp.py
 
 #svm trials tstat
 sed -e s:"imDat    = 'cope'":"imDat    = 'tstat'":g \
@@ -24,7 +24,6 @@ sed -e s:"imDat    = 'cope'":"imDat    = 'tstat'":g \
 python ${tmpScrDir}/mvpa_memsamp1.py
 
 #crossnobis block
-sed -e s:"imDat    = 'cope'":"imDat    = 'tstat'":g \
-    -e s:"distMeth = 'svm'":"distMeth = 'crossNobis'":g \
-  < ${codeDir}/mvpa_memsamp_blocks.py > ${tmpScrDir}/mvpa_memsamp_blocks1.py
-python ${tmpScrDir}/mvpa_memsamp_blocks1.py
+# sed -e s:"distMeth = 'svm'":"distMeth = 'crossNobis'":g \
+#   < ${codeDir}/mvpa_memsamp_blocks.py > ${tmpScrDir}/mvpa_memsamp_blocks1.py
+# python ${tmpScrDir}/mvpa_memsamp_blocks1.py
--- run_searchlight_1.sh
+++ run_searchlight_1.sh
@@ -75,6 +75,16 @@ sed -e s:"#mainDir":"mainDir":g \
   < ${codeDir}/mvpa_searchlight_memsamp_blocks.py > ${tmpScrDir}/mvpa_searchlight_memsamp_blocks1.py
 python ${tmpScrDir}/mvpa_searchlight_memsamp_blocks1.py
 
+#above didin't actually run 1:19... - rerun!
+#12-way sl6 niNorm svm tstat blocks
+sed -e s:"#mainDir":"mainDir":g \
+    -e s:"for iSub in range(1,34)":"for iSub in range(1,19)":g \
+    -e s:"decodeFeature = 'subjCat'":"decodeFeature = '12-way'":g \
+    -e s:"normMeth = 'noNorm'":"normMeth = 'niNormalised'":g \
+    -e s:"imDat   = 'cope'":"imDat   = 'tstat'":g \
+  < ${codeDir}/mvpa_searchlight_memsamp_blocks.py > ${tmpScrDir}/mvpa_searchlight_memsamp_blocks1.py
+python ${tmpScrDir}/mvpa_searchlight_memsamp_blocks1.py
+
 
 
 
