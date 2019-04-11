47271f4c76dd23fac30f182b57442453c5c01ffd
Robert Mok <robertmok@Roberts-MacBook-Pro.local>
2019-04-11 18:26:03 +0100

    update

--- plot_roi_memsamp.py
+++ plot_roi_memsamp.py
@@ -14,10 +14,13 @@ import pandas as pd
 roiDir='/Users/robert.mok/Documents/Postdoc_ucl/memsamp_fMRI/mvpa_roi/'
 #roiDir='/Users/robert.mok/Documents/Postdoc_ucl/memsamp_fMRI/mvpa_roi/bilateral'
 
+# laptop
+roiDir='/Users/robertmok/Documents/Postdoc_ucl/mvpa_roi/' 
+
 imDat    = 'cope' # cope or tstat images
 normMeth = 'noNorm' # 'niNormalised', 'demeaned', 'demeaned_stdNorm', 'noNorm' # demeaned_stdNorm - dividing by std does work atm
-distMeth = 'svm' # 'svm', 'crossNobis'
-trainSetMeth = 'trials' # 'trials' or 'block' 
+distMeth = 'crossNobis' # 'svm', 'crossNobis'
+trainSetMeth = 'block' # 'trials' or 'block' 
 fwhm = None # optional smoothing param - 1, or None
 
 decodeFeature = 'subjCat' # '12-way' (12-way dir decoding - only svm), 'dir' (opposite dirs), 'ori' (orthogonal angles)
@@ -34,6 +37,6 @@ df=pd.read_pickle((os.path.join(roiDir, 'roi_' + decodeFeature + 'Decoding_' +
 
 
 stdAll = df.iloc[0:33,:].std()/np.sqrt(33)
-ax=df.iloc[0:33,:].mean().plot(figsize=(15,5),kind="bar",yerr=stdAll)
+ax=df.iloc[0:33,:].mean().plot(figsize=(10,5),kind="bar",yerr=stdAll)
 #ax=df.iloc[0:33,:].mean().plot(figsize=(15,5),kind="bar",yerr=stdAll,ylim=(.5,.537))
 #ax=df.iloc[0:33,:].mean().plot(figsize=(15,5),kind="bar",yerr=stdAll,ylim=(1/12,0.09))
