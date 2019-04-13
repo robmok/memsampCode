abf30414accc8c0e69d62a7c13d43873d98508fe
Robert Mok <robert.mok@love06.local>
2019-04-13 18:53:03 +0100

    update

--- run_mvpa_1.sh
+++ run_mvpa_1.sh
@@ -23,36 +23,4 @@ sed -e s:"#mainDir":"mainDir":g \
   < ${codeDir}/mvpa_memsamp.py > ${tmpScrDir}/mvpa_memsamp1.py
 python ${tmpScrDir}/mvpa_memsamp1.py
 
-# ori crossnobis block
-sed -e s:"#mainDir":"mainDir":g \
-    -e s:"decodeFeature = 'subjCat'":"decodeFeature = 'ori'":g \
-    -e s:"distMeth = 'svm'":"distMeth = 'crossNobis'":g \
-  < ${codeDir}/mvpa_memsamp_blocks.py > ${tmpScrDir}/mvpa_memsamp_blocks1.py
-python ${tmpScrDir}/mvpa_memsamp_blocks1.py
-
-#ori svm block tstat
-sed -e s:"#mainDir":"mainDir":g \
-    -e s:"decodeFeature = 'subjCat'":"decodeFeature = 'ori'":g \
-    -e s:"imDat    = 'cope'":"imDat    = 'tstat'":g \
-  < ${codeDir}/mvpa_memsamp_blocks.py > ${tmpScrDir}/mvpa_memsamp_blocks1.py
-python ${tmpScrDir}/mvpa_memsamp_blocks1.py
-
-
-
 # ori crossnobis trials - to run? not run yet
-
-
-
-
-
-
-# # #12-way blocks - extras
-# #svm copes
-# sed -e s:"decodeFeature = 'ori'":"decodeFeature = '12-way'":g \
-#   < ${codeDir}/mvpa_memsamp_blocks.py > ${tmpScrDir}/mvpa_memsamp_blocks1.py
-# python ${tmpScrDir}/mvpa_memsamp_blocks1.py
-# #svm tstat
-# sed -e s:"decodeFeature = 'ori'":"decodeFeature = '12-way'":g \
-#     -e s:"imDat    = 'cope'":"imDat    = 'tstat'":g \
-#   < ${codeDir}/mvpa_memsamp_blocks.py > ${tmpScrDir}/mvpa_memsamp_blocks1.py
-# python ${tmpScrDir}/mvpa_memsamp_blocks1.py
