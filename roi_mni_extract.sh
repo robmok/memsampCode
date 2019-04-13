0c2a5b51f569f4ca7894e982345b1dbb05794dde
Robert Mok <robert.mok@love06.local>
2019-04-13 19:34:20 +0100

    update

--- roi_mni_extract.sh
+++ roi_mni_extract.sh
@@ -12,3 +12,16 @@ for iRoi in {1..25}; do
   fslmaths ${roiDir}/maxprob_vol_lh.nii.gz -thr ${iRoi} -uthr ${iRoi} -bin -s 0.25 -bin ${roiDirOut}/roi${iRoi}_lh.nii.gz
   fslmaths ${roiDir}/maxprob_vol_rh.nii.gz -thr ${iRoi} -uthr ${iRoi} -bin -s 0.25 -bin ${roiDirOut}/roi${iRoi}_rh.nii.gz
 done
+
+
+
+# rm ${roiDir}/sl_rois_mni.txt
+# #note - used MD ips here, not wang/kastner
+# for iRoi in 'V1v' 'V1d'  'V2v' 'V2d'  'V3v'  'V3d'  'hV4' 'MST' 'hMT' 'V3b' 'V3a' 'IPS0' 'HIPP_HEAD_BODY_TAIL'; do
+#   echo -n "${roiDir}/${subject}_${iRoi}_lh.nii.gz -add " >> ${roiDir}/sl_rois_mni.txt #note the -n (for no new line) and space at the end of ""
+#   echo -n "${roiDir}/${subject}_${iRoi}_rh.nii.gz -add " >> ${roiDir}/sl_rois_mni.txt
+# done
+# iRoi='MDroi_all' #last no '-add' - no lh/rh
+# echo -n "${roiDir}/${subject}_${iRoi}.nii.gz " >> ${roiDir}/sl_rois_mni.txt
+# roiList=`cat ${roiDir}/sl_rois_mni.txt`
+# fslmaths ${roiList} -bin ${roiDir}/allROIsSL.nii.gz
