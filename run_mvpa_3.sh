d686cb89a8c26459525c8a2e00bcbbd0620a0085
Robert Mok <robert.mok@love06.local>
2019-04-15 16:50:21 +0100

    update

--- run_mvpa_3.sh
+++ run_mvpa_3.sh
@@ -8,7 +8,8 @@ codeDir=${mainDir}/'memsampCode'
 tmpScrDir=${mainDir}/'mvpaTmpScripts'
 
 #objCat
-sed -e s:"decodeFeature = 'subjCat'":"decodeFeature = 'objCat'":g \
+sed -e s:"#mainDir":"mainDir":g \
+    -e s:"decodeFeature = 'subjCat'":"decodeFeature = 'objCat'":g \
     -e s:"imDat    = 'cope'":"imDat    = 'tstat'":g \
     < ${codeDir}/mvpa_memsamp_blocks.py > ${tmpScrDir}/mvpa_memsamp_blocks1.py
 python ${tmpScrDir}/mvpa_memsamp_blocks1.py
