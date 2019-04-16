7a6da00906689d254804e54c34262d58c5484239
Robert Mok <robert.mok@love06.local>
2019-04-16 11:15:34 +0100

    update

--- run_searchlight_2.sh
+++ run_searchlight_2.sh
@@ -11,12 +11,14 @@ tmpScrDir=${mainDir}/'mvpaTmpScripts'
 #Blocks
 #crossnobis
 sed -e s:"#mainDir":"mainDir":g \
+    -e s:"for iSub in range(1,nSubs+1)":"for iSub in range(4,nSubs+1)":g \
     -e s:"distMeth = 'svm'":"distMeth = 'crossNobis'":g \
   < ${codeDir}/mvpa_searchlight_memsamp_blocks.py > ${tmpScrDir}/mvpa_searchlight_memsamp_blocks1.py
 python ${tmpScrDir}/mvpa_searchlight_memsamp_blocks1.py
 
 #ori
 sed -e s:"#mainDir":"mainDir":g \
+    -e s:"for iSub in range(1,nSubs+1)":"for iSub in range(4,nSubs+1)":g \
     -e s:"distMeth = 'svm'":"distMeth = 'crossNobis'":g \
     -e s:"decodeFeature = 'subjCat'":"decodeFeature = 'ori'":g \
   < ${codeDir}/mvpa_searchlight_memsamp_blocks.py > ${tmpScrDir}/mvpa_searchlight_memsamp_blocks1.py
