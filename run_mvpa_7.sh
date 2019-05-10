12c4783afe6c46ffac17f3009b4afd57025da324
Robert Mok <robert.mok@love06.local>
2019-05-10 12:16:24 +0100

    update

--- run_mvpa_7.sh
+++ run_mvpa_7.sh
@@ -10,24 +10,25 @@ tmpScrDir=${mainDir}/'mvpaTmpScripts'
 
 #dCentred
 
-# 12-way cope trials
-sed -e s:"decodeFeature = 'subjCat'":"decodeFeature = '12-way'":g \
-    -e s:"normMeth = 'noNorm'":"normMeth = 'dCentred'":g \
-    -e s:"#mainDir":"mainDir":g \
-    < ${codeDir}/mvpa_memsamp.py > ${tmpScrDir}/mvpa_memsamp1.py
-python ${tmpScrDir}/mvpa_memsamp1.py
-
-# 12-way tstat trials
-sed -e s:"decodeFeature = 'subjCat'":"decodeFeature = '12-way'":g \
-    -e s:"normMeth = 'noNorm'":"normMeth = 'dCentred'":g \
-    -e s:"imDat = 'cope'":"imDat = 'tstat'":g \
-    -e s:"#mainDir":"mainDir":g \
-    < ${codeDir}/mvpa_memsamp.py > ${tmpScrDir}/mvpa_memsamp1.py
-python ${tmpScrDir}/mvpa_memsamp1.py
+# # 12-way cope trials
+# sed -e s:"decodeFeature = 'subjCat'":"decodeFeature = '12-way'":g \
+#     -e s:"normMeth = 'noNorm'":"normMeth = 'dCentred'":g \
+#     -e s:"#mainDir":"mainDir":g \
+#     < ${codeDir}/mvpa_memsamp.py > ${tmpScrDir}/mvpa_memsamp1.py
+# python ${tmpScrDir}/mvpa_memsamp1.py
+#
+# # 12-way tstat trials
+# sed -e s:"decodeFeature = 'subjCat'":"decodeFeature = '12-way'":g \
+#     -e s:"normMeth = 'noNorm'":"normMeth = 'dCentred'":g \
+#     -e s:"imDat = 'cope'":"imDat = 'tstat'":g \
+#     -e s:"#mainDir":"mainDir":g \
+#     < ${codeDir}/mvpa_memsamp.py > ${tmpScrDir}/mvpa_memsamp1.py
+# python ${tmpScrDir}/mvpa_memsamp1.py
 
 # 12-way cope block
 sed -e s:"decodeFeature = 'subjCat'":"decodeFeature = '12-way'":g \
     -e s:"normMeth = 'noNorm'":"normMeth = 'dCentred'":g \
+    -e s:"#mainDir":"mainDir":g \
     < ${codeDir}/mvpa_memsamp_blocks.py > ${tmpScrDir}/mvpa_memsamp_blocks1.py
 python ${tmpScrDir}/mvpa_memsamp_blocks1.py
 
@@ -35,6 +36,7 @@ python ${tmpScrDir}/mvpa_memsamp_blocks1.py
 sed -e s:"decodeFeature = 'subjCat'":"decodeFeature = '12-way'":g \
     -e s:"imDat = 'cope'":"imDat = 'tstat'":g \
     -e s:"normMeth = 'noNorm'":"normMeth = 'dCentred'":g \
+    -e s:"#mainDir":"mainDir":g \
     < ${codeDir}/mvpa_memsamp_blocks.py > ${tmpScrDir}/mvpa_memsamp_blocks1.py
 python ${tmpScrDir}/mvpa_memsamp_blocks1.py
 
