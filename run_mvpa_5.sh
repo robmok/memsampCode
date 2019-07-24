5034ff468cc3ec2eba8e08493c91cce1db60ff74
Robert Mok <robert.mok@love06.local>
2019-07-24 12:06:42 +0100

    update

--- run_mvpa_5.sh
+++ run_mvpa_5.sh
@@ -7,12 +7,9 @@ mainDir='/home/robmok/Documents/memsamp_fMRI' #love01
 codeDir=${mainDir}/'memsampCode'
 tmpScrDir=${mainDir}/'mvpaTmpScripts'
 
-#trying svms C=1, rois 0.25
+#rerunning, with SPL1
 
-#love01
-#subjCat, subjCat-orth, subjCat-orth-ctrl, subjCat-all (svm), 12-way
-
-#subjCat
-sed -e s:"#mainDir":"mainDir":g \
+sed -e s:"decodeFeature = 'subjCat'":"decodeFeature = '12-way'":g \
+    -e s:"#mainDir":"mainDir":g \
     < ${codeDir}/mvpa_memsamp.py > ${tmpScrDir}/mvpa_memsamp1.py
 python ${tmpScrDir}/mvpa_memsamp1.py
